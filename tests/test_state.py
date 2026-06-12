3a857d8182aeb1acb0d3a845a1061afc4fc9e963
irisdigitllab <info@irisdigitallab.com>
2026-06-12 01:59:48 -0400

    feat: estado ARMED y sesiones manos libres en la maquina de estados

--- tests/test_state.py
+++ tests/test_state.py
@@ -49,3 +49,96 @@ def test_error_y_recuperacion_con_click():
     assert sm.state is State.ERROR
     sm.click()
     assert sm.state is State.IDLE
+
+
+def make_sm_ml():
+    sm = StateMachine(handsfree_enabled=True)
+    events = []
+    sm.on_change.append(lambda s: events.append(s))
+    sm.on_start_listening.append(lambda: events.append("START_REC"))
+    sm.on_stop_listening.append(lambda: events.append("STOP_REC"))
+    sm.on_cancel_listening.append(lambda: events.append("CANCEL_REC"))
+    return sm, events
+
+
+def test_model_ready_va_a_armed_con_manos_libres():
+    sm, _ = make_sm_ml()
+    sm.model_ready()
+    assert sm.state is State.ARMED
+
+
+def test_wake_inicia_sesion_manos_libres():
+    sm, events = make_sm_ml()
+    sm.model_ready()
+    sm.wake_detected()
+    assert sm.state is State.LISTENING
+    assert sm.session_handsfree is True
+    assert "START_REC" in events
+
+
+def test_click_desde_armed_inicia_sesion_manual():
+    sm, _ = make_sm_ml()
+    sm.model_ready()
+    sm.click()
+    assert sm.state is State.LISTENING
+    assert sm.session_handsfree is False
+
+
+def test_silencio_solo_corta_sesiones_manos_libres():
+    sm, _ = make_sm_ml()
+    sm.model_ready()
+    sm.click()             # sesión manual
+    sm.silence_detected()  # ignorado
+    assert sm.state is State.LISTENING
+
+
+def test_silencio_corta_sesion_wake():
+    sm, _ = make_sm_ml()
+    sm.model_ready()
+    sm.wake_detected()
+    sm.silence_detected()
+    assert sm.state is State.TRANSCRIBING
+
+
+def test_wake_ignorado_fuera_de_armed():
+    sm, _ = make_sm_ml()
+    sm.model_ready()
+    sm.wake_detected()
+    sm.wake_detected()  # ya escuchando: ignorado
+    assert sm.state is State.LISTENING
+
+
+def test_cancel_vuelve_a_armed_sin_transcribir():
+    sm, events = make_sm_ml()
+    sm.model_ready()
+    sm.wake_detected()
+    sm.cancel()
+    assert sm.state is State.ARMED
+    assert "CANCEL_REC" in events
+    assert "STOP_REC" not in events
+
+
+def test_toggle_handsfree_alterna_idle_armed():
+    sm, _ = make_sm_ml()
+    sm.model_ready()
+    sm.toggle_handsfree()
+    assert sm.state is State.IDLE
+    sm.toggle_handsfree()
+    assert sm.state is State.ARMED
+
+
+def test_transcription_done_vuelve_a_armed():
+    sm, _ = make_sm_ml()
+    sm.model_ready()
+    sm.wake_detected()
+    sm.silence_detected()
+    sm.transcription_done()
+    assert sm.state is State.ARMED
+
+
+def test_error_se_recupera_a_armed_con_click():
+    sm, _ = make_sm_ml()
+    sm.model_ready()
+    sm.fail()
+    sm.click()
+    assert sm.state is State.ARMED
